0961ad975949a06d1db050595fdc2e4929ec810f
marketinginternouniurb-byte <marketing.internouniurb@gmail.com>
2026-05-26 16:18:54 -0300

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from '@tanstack/start/config'
+import { defineConfig } from '@tanstack/react-start/config'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   server: {
     preset: 'vercel',
   },
   vite: {
-    plugins: [],
+    plugins: [tsconfigPaths()],
   },
 })
