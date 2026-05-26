5b1a53a6263dbe00d2d910572eba2e2900f87b5f
marketinginternouniurb-byte <marketing.internouniurb@gmail.com>
2026-05-26 16:13:30 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
+import { tanstackStart } from '@tanstack/start/plugin/vite'
 
 export default defineConfig({
   plugins: [
-    react(),
+    tanstackStart(),
     tsconfigPaths(),
     tailwindcss(),
   ],
