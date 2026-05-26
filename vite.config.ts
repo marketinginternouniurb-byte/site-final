58192d48b96baf5e4f6d6f608cf9db71f07a94e5
marketinginternouniurb-byte <marketing.internouniurb@gmail.com>
2026-05-26 16:20:41 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
-import { defineConfig } from '@tanstack/react-start/config'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  server: {
-    preset: 'vercel',
+  tanstackStart: {
+    server: { entry: "server", preset: "vercel" },
   },
-  vite: {
-    plugins: [tsconfigPaths()],
-  },
-})
+});
