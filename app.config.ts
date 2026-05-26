bd28c947d5a7225459cbe9efb617bc617b77c72f
Remo AlvarengaPedro <marketing.internouniurb@gmail.com>
2026-05-26 15:33:25 -0300

    Adicionando vercel.json de roteamento

--- app.config.ts
+++ app.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from '@tanstack/start/config'
 
 export default defineConfig({
   server: {
-    preset: 'vercel', // Isso fará o Nitro (motor do TanStack) criar as funções da Vercel
+    preset: 'vercel',
   },
 })
