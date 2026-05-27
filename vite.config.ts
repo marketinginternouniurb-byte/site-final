ef8a305114ec16cc0a629899ea3730964b7155a3
Remo AlvarengaPedro <marketing.internouniurb@gmail.com>
2026-05-27 15:59:15 -0300

    Reapply "fix:atualização 2.0"
    This reverts commit fb11c4b1d3a0cfd52d04acef031f02f6d2d9af0e.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import viteTsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths()],
-})
+  tanstackStart: {
+    server: { preset: "vercel" },
+  },
+});
